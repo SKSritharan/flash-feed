f972f3697dad0784ff68dfa73074b94d510ac584
Sritharan Thiyagarajah <sk.sritharan75@gmail.com>
2024-02-15 08:18:01 +0530

    App bar fixed

--- components/UI/AppBar.js
+++ components/UI/AppBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Switch,
   Text,
@@ -7,9 +7,9 @@ import {
   View,
   StyleSheet,
 } from "react-native";
-import { BellIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
-import { useNavigation } from "@react-navigation/native";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import { useColorScheme } from "nativewind";
 
 export default function AppBar() {
@@ -17,60 +17,60 @@ export default function AppBar() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
 
   return (
-    <View className="flex-row justify-between items-center mx-4 mt-4">
-      <View className="">
-        <Text
-          className="font-spaceGroteskBold text-2xl text-green-800 dark:text-white font-extrabold uppercase"
-          style={{
-            fontFamily: "SpaceGroteskBold",
-          }}
-        >
-          Flash Feed
-        </Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Flash Feed</Text>
 
-      <View
-        className="flex-row space-x-4 rounded-full justify-center items-center"
-        style={{ flexDirection: "row", justifyContent: "center" }}
-      >
+      <View style={styles.iconContainer}>
         <TouchableOpacity
           onPress={() => navigation.navigate("Search")}
-          className="bg-gray-200 dark:bg-red-800  rounded-full p-2"
+          style={styles.searchButton}
         >
-          <View
-            className="flex-row items-center"
-            style={{ flexDirection: "row" }}
-          >
-            <TextInput
-              style={{
-                height: 40,
-                borderColor: "gray",
-                borderWidth: 1,
-                borderRadius: 20,
-                paddingLeft: 15,
-                marginRight: 10,
-                color: colorScheme == "dark" ? "white" : "black",
-              }}
-              placeholder="Search"
-              placeholderTextColor={colorScheme == "dark" ? "white" : "gray"}
-            />
-
+          <View style={styles.searchInputContainer}>
             <MagnifyingGlassIcon
               size={25}
               strokeWidth={2}
-              color={colorScheme == "dark" ? "white" : "red"}
+              color={colorScheme == "dark" ? "white" : "black"}
             />
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={toggleColorScheme}>
           {colorScheme == "dark" ? (
-            <Ionicons name="sunny" size={25} strokeWidth={2} color="yellow" />
+            <Ionicons name="sunny" size={25} color="yellow" />
           ) : (
-            <Ionicons name="moon" size={25} strokeWidth={2} color="gray" />
+            <Ionicons name="moon" size={25} color="gray" />
           )}
         </TouchableOpacity>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 16,
+    marginTop: 16,
+  },
+  title: {
+    fontFamily: "SpaceGroteskBold",
+    fontSize: 20,
+    color: "#FF3A44",
+  },
+  iconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  searchButton: {
+    borderRadius: 20,
+    padding: 2,
+    marginLeft: 12,
+  },
+  searchInputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 12,
+  },
+});
